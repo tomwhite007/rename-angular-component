750b6d4b965f689fa06c9cb992979f28d0e854c1
Tom White <tomwhite007@gmail.com>
2021-07-23 09:22:02 +0100

    input box with filename

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { FileHandle } from 'fs/promises';
+import { FileInfoResult } from 'prettier';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -16,8 +18,22 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let renameComponent = vscode.commands.registerCommand(
     'rename-angular-component.renameComponent',
-    () => {
-      // The code you place here will be executed every time your command is executed
+    (e) => {
+      const filePath: string = e.path;
+      const lastSlash = filePath.lastIndexOf('/');
+      const path = filePath.substr(0, lastSlash);
+      const file = filePath.substr(
+        lastSlash + 1,
+        filePath.length - lastSlash - 1
+      );
+      console.log(path, file);
+
+      vscode.window.showInputBox({
+        title: 'Rename file stub',
+        prompt:
+          'Type the new filename stub you want to rename to. Use kebab case.',
+        value: file,
+      });
       // Display a message box to the user
       vscode.window.showInformationMessage(
         'Hello World from Rename Angular Component!'

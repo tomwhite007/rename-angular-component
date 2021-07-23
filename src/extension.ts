3f599c71e4eb52a76be201e54b1508c161a26762
Tom White <tomwhite007@gmail.com>
2021-07-23 07:14:14 +0100

    rename type sensitive title

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  let renameComponent = vscode.commands.registerCommand(
     'rename-angular-component.renameComponent',
     () => {
       // The code you place here will be executed every time your command is executed
@@ -24,8 +24,31 @@ export function activate(context: vscode.ExtensionContext) {
       );
     }
   );
+  context.subscriptions.push(renameComponent);
 
-  context.subscriptions.push(disposable);
+  let renameDirective = vscode.commands.registerCommand(
+    'rename-angular-component.renameDirective',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        'Hello World from Rename Angular Directive!'
+      );
+    }
+  );
+  context.subscriptions.push(renameDirective);
+
+  let renameService = vscode.commands.registerCommand(
+    'rename-angular-component.renameService',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        'Hello World from Rename Angular Service!'
+      );
+    }
+  );
+  context.subscriptions.push(renameService);
 }
 
 // this method is called when your extension is deactivated

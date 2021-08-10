a9f534ca062831725c33e9c5d4f79ed484a224e7
Tom White <tomwhite007@gmail.com>
2021-08-10 08:18:39 +0100

    feat: file renames working

--- src/rename-helpers.ts
+++ src/rename-helpers.ts
@@ -3,6 +3,7 @@ import { camelCase } from 'camel-case';
 import { pascalCase } from 'pascal-case';
 import { paramCase } from 'param-case';
 import * as fs from 'fs';
+import path = require('path');
 
 export type AngularConstruct = 'component' | 'directive' | 'service' | 'module';
 export interface OriginalFileDetails {
@@ -10,13 +11,13 @@ export interface OriginalFileDetails {
   file: string;
   stub: string;
 }
-const componentRegex = /^.+\.component\.(spec.ts|scss|html|ts)$/;
-const directiveRegex = /^.+\.directive\.(spec.ts|ts)$/;
-const serviceRegex = /^.+\.service\.(spec.ts|ts)$/;
-const likeFilesRegexLookup: { [key: string]: RegExp } = {
-  component: componentRegex,
-  directive: directiveRegex,
-  service: serviceRegex,
+const componentRegexPartial = `\.component\.(spec.ts|scss|html|ts)$`;
+const directiveRegexPartial = `\.directive\.(spec.ts|ts)$`;
+const serviceRegexPartial = `\.service\.(spec.ts|ts)$`;
+const likeFilesRegexPartialLookup: { [key: string]: string } = {
+  component: componentRegexPartial,
+  directive: directiveRegexPartial,
+  service: serviceRegexPartial,
 };
 
 export function rename(construct: AngularConstruct, filePath: string) {
@@ -30,10 +31,8 @@ export function rename(construct: AngularConstruct, filePath: string) {
       value: fileDetails.stub,
     })
     .then((newStub) => renameToNewStub(construct, newStub, fileDetails));
-  // Display a message box to the user
-  vscode.window.showInformationMessage(
-    `FAKE Successfully renamed ${construct}`
-  );
+
+  // TODO: need to validate stub for illegal chars and let user know slashes not allowed
 }
 
 function originalFileDetails(filePath: string): OriginalFileDetails {
@@ -54,40 +53,57 @@ function renameToNewStub(
   // component = 4 files, directive|service = 2 files to rename
 
   if (!newStub) {
-    vscode.window.showInformationMessage(
-      'Empty new name entered. No files changed.'
-    );
+    return logInfo('. Empty new name entered. No files changed.', construct);
+  }
+  if (newStub === selectedFileDetails.stub) {
+    return logInfo('. No files changed.', construct);
     return;
   }
-
   // make sure it's kebab
   newStub = paramCase(newStub);
 
-  // select the right regex
-  const likeFilesRegex = likeFilesRegexLookup[construct];
-
-  const foundFilesToRename: string[] = fs
-    .readdirSync(selectedFileDetails.path)
-    .filter(
-      (file: string) =>
-        likeFilesRegex.test(file) && file.startsWith(selectedFileDetails.stub)
-    );
-
-  foundFilesToRename.forEach((foundFile) => {
-    const newFilename = foundFile.replace(
-      RegExp(`^${selectedFileDetails.stub}`),
-      <string>newStub
-    );
-    fs.renameSync(
-      `${selectedFileDetails.path}/${foundFile}`,
-      `${selectedFileDetails.path}/${newFilename}`
-    );
-  });
-
-  vscode.window.showInformationMessage(
-    `Successfully renamed!
-    How about that?`
+  // rename folder
+  const { newPath, renameFolderErrorMsgs } = renameFolder(
+    selectedFileDetails.stub,
+    newStub,
+    selectedFileDetails.path
   );
+  selectedFileDetails.path = newPath;
+
+  // find files to rename
+  const { foundFilesToRename, findFileErrorMsgs } = findFilesToRename(
+    selectedFileDetails.path,
+    selectedFileDetails.stub,
+    construct
+  );
+  if (findFileErrorMsgs.length) {
+    return logErrors(construct, [
+      ...renameFolderErrorMsgs,
+      ...findFileErrorMsgs,
+    ]);
+  }
+
+  // rename each file
+  const { renamedFiles, renameFilesErrorMsgs } = renameEachFile(
+    foundFilesToRename,
+    selectedFileDetails.stub,
+    newStub,
+    selectedFileDetails.path
+  );
+  if (renameFilesErrorMsgs.length) {
+    return logErrors(construct, [
+      ...renameFolderErrorMsgs,
+      ...renameFilesErrorMsgs,
+    ]);
+  }
+
+  // TODO: rename filename and folder import paths
+
+  // TODO: rename Class name
+  // TODO: rename Class imports
+
+  // TODO: rename Selector
+  // TODO: rename Selector where used in templates
 
   // console.log('camelCase', camelCase(stub));
   // console.log('pascalCase', pascalCase(stub));
@@ -95,10 +111,105 @@ function renameToNewStub(
 
   // console.log(vscode.workspace.getWorkspaceFolder({j}))
 
-  // var fs = require('fs');
-  // fs.rename(filePath, `${path}/test.txt`, function (err: string) {
-  //   if (err) {
-  //     console.log('ERROR: ' + err);
-  //   }
-  // });
+  logInfo(' success', construct, renameFolderErrorMsgs);
+}
+
+function findFilesToRename(
+  path: string,
+  stub: string,
+  construct: AngularConstruct
+): { foundFilesToRename: string[]; findFileErrorMsgs: string[] } {
+  // build like-files regex
+  const likeFilesRegex = RegExp(
+    `^${stub}${likeFilesRegexPartialLookup[construct]}`
+  );
+  let foundFilesToRename: string[] = [];
+  let findFileErrorMsgs: string[] = [];
+  try {
+    // find files to rename
+    foundFilesToRename = fs
+      .readdirSync(path)
+      .filter((file: string) => likeFilesRegex.test(file));
+  } catch {
+    findFileErrorMsgs = ['Finding files to rename after selection failed.'];
+  }
+
+  return {
+    foundFilesToRename,
+    findFileErrorMsgs,
+  };
+}
+
+function renameFolder(
+  oldStub: string,
+  newStub: string,
+  path: string
+): { newPath: string; renameFolderErrorMsgs: string[] } {
+  let newPath: string = '';
+  let renameFolderErrorMsgs: string[] = [];
+  const folderRegex = RegExp(`/${oldStub}$`);
+
+  if (path.match(folderRegex)) {
+    newPath = path.replace(folderRegex, `/${newStub}`);
+    try {
+      fs.renameSync(path, newPath);
+    } catch (e) {
+      renameFolderErrorMsgs = [
+        `There was a file sytem error attempting to rename folder.`,
+      ];
+    }
+  } else {
+    newPath = path;
+    renameFolderErrorMsgs = [`Unable to rename containing folder.`];
+  }
+
+  return { newPath, renameFolderErrorMsgs };
+}
+
+function renameEachFile(
+  foundFilesToRename: string[],
+  stub: string,
+  newStub: string,
+  path: string
+): { renamedFiles: string[]; renameFilesErrorMsgs: string[] } {
+  const renamedFiles: string[] = [];
+  const renameFilesErrorMsgs: string[] = [];
+
+  foundFilesToRename.forEach((foundFile) => {
+    const newFilename = foundFile.replace(RegExp(`^${stub}`), <string>newStub);
+
+    try {
+      const newFilePath = `${path}/${newFilename}`;
+      fs.renameSync(`${path}/${foundFile}`, newFilePath);
+      renamedFiles.push(newFilePath);
+    } catch {
+      renameFilesErrorMsgs.push(
+        `Rename ${foundFile} to ${newFilename} failed.`
+      );
+    }
+  });
+
+  return { renamedFiles, renameFilesErrorMsgs };
+}
+
+function logErrors(construct: AngularConstruct, textLines?: string[]) {
+  logInfo(' failed. See output tab for more details.', construct, textLines);
+}
+
+function logInfo(
+  additionalInfoMessage: string,
+  construct: AngularConstruct,
+  textLines?: string[]
+) {
+  const title = `Rename Angular ${pascalCase(construct)}`;
+  vscode.window.showInformationMessage(`${title}${additionalInfoMessage}`);
+
+  if (textLines) {
+    const channel = vscode.window.createOutputChannel(title);
+    channel.appendLine(``);
+    channel.appendLine(`*** ${title} output: ***`);
+    channel.appendLine(``);
+    textLines.forEach((line) => channel.appendLine(line));
+    channel.show();
+  }
 }

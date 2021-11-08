ece0c26e4f67679f80c64cecbb59dc85e35b4743
Tom White <tomwhite007@gmail.com>
2021-11-08 07:46:13 +0000

    fix: typo and bump

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { FileHandle } from "fs/promises";
-import { FileInfoResult } from "prettier";
-import * as vscode from "vscode";
-import { rename } from "./helpers/rename.function";
+import { FileHandle } from 'fs/promises';
+import * as vscode from 'vscode';
+import { rename } from './helpers/rename.function';
 
 export function activate(context: vscode.ExtensionContext) {
   let renameComponent = vscode.commands.registerCommand(
-    "rename-angular-component.renameComponent",
-    (uri: vscode.Uri) => rename("component", uri)
+    'rename-angular-component.renameComponent',
+    (uri: vscode.Uri) => rename('component', uri)
   );
   context.subscriptions.push(renameComponent);
 
   let renameDirective = vscode.commands.registerCommand(
-    "rename-angular-component.renameDirective",
-    (uri: vscode.Uri) => rename("directive", uri)
+    'rename-angular-component.renameDirective',
+    (uri: vscode.Uri) => rename('directive', uri)
   );
   context.subscriptions.push(renameDirective);
 
   let renameService = vscode.commands.registerCommand(
-    "rename-angular-component.renameService",
-    (uri: vscode.Uri) => rename("service", uri)
+    'rename-angular-component.renameService',
+    (uri: vscode.Uri) => rename('service', uri)
   );
   context.subscriptions.push(renameService);
 }

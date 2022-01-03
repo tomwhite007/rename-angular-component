42d09f94076e529bcdcae3fe518a8067c7d7b5cf
Tom White <tomwhite007@gmail.com>
2022-01-03 07:51:10 +0000

    refactor: move initialisation into setRenameDetails

--- src/rename-angular-component/renamer.class.ts
+++ src/rename-angular-component/renamer.class.ts
@@ -25,95 +25,28 @@ import { EXTENSION_NAME } from './definitions/extension-name';
 import { noSelectedFileHandler } from './no-selected-file-handler/no-selected-file-handler.function';
 import { getOriginalClassName } from './in-file-edits/get-original-class-name.function';
 
+const timeoutPause = async (wait = 0) => {
+  await new Promise((res) => setTimeout(res, wait));
+  return;
+};
+
 export class Renamer {
+  readonly constructsWithSelectors = ['component', 'directive'];
   private construct!: AngularConstruct;
-  private uri!: vscode.Uri;
   private title!: string;
+  private originalFileDetails!: Readonly<OriginalFileDetails>;
+  private projectRoot!: string;
+  private newStub!: string;
+  private processTimerStart!: number;
 
   constructor(
-    private importer: ReferenceIndexer,
+    private indexer: ReferenceIndexer,
     private indexerInitialisePromise: Thenable<any>,
     private userMessage: UserMessage
   ) {}
 
-  async rename(_construct: AngularConstruct, _uri: vscode.Uri) {
-    this.construct = _construct;
-    this.uri = _uri;
-    this.title = `Rename Angular ${pascalCase(this.construct)}`;
-
-    // Handle if called from command menu
-    if (!this.uri) {
-      const userEntered = await noSelectedFileHandler(
-        this.construct,
-        this.title,
-        this.userMessage
-      );
-      if (userEntered) {
-        this.uri = userEntered;
-      } else {
-        return;
-      }
-    }
-
-    const originalFileDetails: Readonly<OriginalFileDetails> =
-      getOriginalFileDetails(this.uri.path);
-    const projectRoot = windowsFilePathFix(getProjectRoot(this.uri) as string);
-    const haveSelectors: Readonly<AngularConstruct[]> = [
-      'component',
-      'directive',
-    ];
-
-    if (checkForOpenUnsavedEditors()) {
-      this.userMessage.popupMessage(
-        `Please save any edits before using ${this.title}`
-      );
-      return;
-    }
-
-    const inputResult = await vscode.window.showInputBox({
-      title: this.title,
-      prompt: `Enter the new ${this.construct} name.`,
-      value: originalFileDetails.stub,
-    });
-    const start = Date.now();
-
-    if (!inputResult) {
-      this.userMessage.popupMessage(
-        `New ${this.construct} name not entered. Stopped.`
-      );
-      return;
-    }
-    if (originalFileDetails.stub === inputResult) {
-      this.userMessage.popupMessage(
-        `${pascalCase(this.construct)} name same as original. Stopped.`
-      );
-      return;
-    }
-    if (!inputResult.match(/^[a-z0-9-_]*$/i)) {
-      this.userMessage.popupMessage(
-        `Currently only supports letters, numbers, dashes and underscore in the new name. (To be improved in next release)`
-      );
-      return;
-    }
-    // make sure it's kebab
-    const newStub = paramCase(inputResult ?? '');
-
-    const timeoutPause = async (wait = 0) => {
-      await new Promise((res) => setTimeout(res, wait));
-      return;
-    };
-
-    this.userMessage.setOperationTitle(this.title);
-
-    // wait for indexer initialise to complete
-    const indexTime = await this.indexerInitialisePromise;
-    this.userMessage.logInfoToChannel(
-      [
-        `Index files completed in ${Math.round(indexTime * 100) / 100} seconds`,
-        '',
-      ],
-      false
-    );
+  async rename(_construct: AngularConstruct, selectedUri: vscode.Uri) {
+    await this.setRenameDetails(_construct, selectedUri);
 
     await vscode.window.withProgress(
       {
@@ -127,18 +60,18 @@ export class Renamer {
 
         try {
           const filesRelatedToStub = await FilesRelatedToStub.init(
-            originalFileDetails,
-            projectRoot,
+            this.originalFileDetails,
+            this.projectRoot,
             this.construct
           );
           const renameFolder = filesRelatedToStub.folderNameSameAsStub;
 
           const filesToMove = filesRelatedToStub.getFilesToMove(
-            newStub as string
+            this.newStub as string
           );
           if (!filesToMove.find((f) => f.isCoreConstruct)) {
             this.userMessage.popupMessage(
-              `The ${this.construct} class file must use the same file naming convention as '${originalFileDetails.file}' for this process to run.`
+              `The ${this.construct} class file must use the same file naming convention as '${this.originalFileDetails.file}' for this process to run.`
             );
             return;
           }
@@ -147,11 +80,11 @@ export class Renamer {
             (f) => f.isCoreConstruct
           )?.filePath;
           const oldClassName = await getOriginalClassName(
-            originalFileDetails.stub,
+            this.originalFileDetails.stub,
             coreFilePath as string,
             this.construct
           );
-          const newClassName = `${pascalCase(newStub)}${pascalCase(
+          const newClassName = `${pascalCase(this.newStub)}${pascalCase(
             this.construct
           )}`;
 
@@ -168,8 +101,8 @@ export class Renamer {
                     getCoreClassEdits(
                       oldClassName,
                       newClassName,
-                      originalFileDetails.stub,
-                      newStub,
+                      this.originalFileDetails.stub,
+                      this.newStub,
                       this.construct,
                       selectorTransfer
                     ))()
@@ -198,16 +131,16 @@ export class Renamer {
 
           const progressIncrement = Math.floor(70 / fileMoveJobs.length);
           let currentProgress = 20;
-          this.importer.startNewMoves(fileMoveJobs);
+          this.indexer.startNewMoves(fileMoveJobs);
           for (const item of fileMoveJobs) {
             currentProgress += progressIncrement;
             progress.report({ increment: currentProgress });
             await timeoutPause(10);
-            await item.move(this.importer);
+            await item.move(this.indexer);
           }
 
           // update selectors for components and directives
-          if (haveSelectors.includes(this.construct)) {
+          if (this.constructsWithSelectors.includes(this.construct)) {
             if (selectorTransfer.oldSelector && selectorTransfer.newSelector) {
               await findReplaceSelectorsInTemplateFiles(
                 selectorTransfer.oldSelector,
@@ -222,49 +155,49 @@ export class Renamer {
           }
 
           /* TODO - big steps left...
-
+ 
         Prep for publish:
-
+ 
           update main readme...
             add issues and link to them
-
+ 
           make repo public & submit extension
-
+ 
           update changelog
-
-
-
+ 
+ 
+ 
         ---- v2 -----
-
+ 
         fix numbers in string to camel case; remove underscore?
-
+ 
         refactor into classes
-
+ 
         replace selector references in (make config option):
           spec
           story and stories files
-
-
+ 
+ 
         handle open editors
           looks like reference indexer, replaceReferences() already can - need same for core class file
             apply edits in dirty editor?
-
+ 
         fix up / remove tsmove conf() configuration
-
+ 
         make sure input newStub matches constraints and formatting allowed by CLI
-
+ 
         refactor for clean classes, functions and pure async await
-
+ 
         ---- v3 -----
         */
-
           // delete original folder
           if (renameFolder) {
-            fs.remove(originalFileDetails.path);
+            fs.remove(this.originalFileDetails.path);
           }
 
           progress.report({ increment: 100 });
-          const renameTime = Math.round((Date.now() - start) / 10) / 100;
+          const renameTime =
+            Math.round((Date.now() - this.processTimerStart) / 10) / 100;
           this.userMessage.logInfoToChannel([
             '',
             `${this.title} completed in ${renameTime} seconds`,
@@ -291,5 +224,81 @@ export class Renamer {
         }
       }
     );
+  }
+
+  private async setRenameDetails(
+    _construct: AngularConstruct,
+    selectedUri: vscode.Uri
+  ): Promise<boolean> {
+    this.construct = _construct;
+    this.title = `Rename Angular ${pascalCase(this.construct)}`;
+
+    // Handle if called from command menu
+    if (!selectedUri) {
+      const userEntered = await noSelectedFileHandler(
+        this.construct,
+        this.title,
+        this.userMessage
+      );
+      if (userEntered) {
+        selectedUri = userEntered;
+      } else {
+        return false;
+      }
+    }
+
+    this.originalFileDetails = getOriginalFileDetails(selectedUri.path);
+    this.projectRoot = windowsFilePathFix(
+      getProjectRoot(selectedUri) as string
+    );
+
+    if (checkForOpenUnsavedEditors()) {
+      this.userMessage.popupMessage(
+        `Please save any edits before using ${this.title}`
+      );
+      return false;
+    }
+
+    const inputResult = await vscode.window.showInputBox({
+      title: this.title,
+      prompt: `Enter the new ${this.construct} name.`,
+      value: this.originalFileDetails.stub,
+    });
+    this.processTimerStart = Date.now();
+
+    if (!inputResult) {
+      this.userMessage.popupMessage(
+        `New ${this.construct} name not entered. Stopped.`
+      );
+      return false;
+    }
+    if (this.originalFileDetails.stub === inputResult) {
+      this.userMessage.popupMessage(
+        `${pascalCase(this.construct)} name same as original. Stopped.`
+      );
+      return false;
+    }
+    if (!inputResult.match(/^[a-z0-9-_]*$/i)) {
+      this.userMessage.popupMessage(
+        `Currently only supports letters, numbers, dashes and underscore in the new name. (To be improved in next release)`
+      );
+      return false;
+    }
+    // make sure it's kebab
+    this.newStub = paramCase(inputResult ?? '');
+
+    this.userMessage.setOperationTitle(this.title);
+
+    // wait for indexer initialise to complete
+    const indexTime = await this.indexerInitialisePromise;
+    this.userMessage.logInfoToChannel(
+      [
+        `Index files completed in ${Math.round(indexTime * 100) / 100} seconds`,
+        '',
+      ],
+      false
+    );
+
+    return true;
   }
 }

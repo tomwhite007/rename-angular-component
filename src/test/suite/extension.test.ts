d63927d0c48acb833a1bcf25b6fe060a69edd00c
Tom White <tomwhite007@gmail.com>
2022-03-11 08:32:02 +0000

    chore: set timeout unlimited

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,24 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { timeoutPause } from '../../utils/timeout-pause';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
+  // this.timeout(0);
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Sample test', async (done) => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+
+    // await vscode.commands.executeCommand(
+    //   'rename-angular-component.renameComponent'
+    // );
+
+    // await timeoutPause(2000);
+
+    console.log('test');
+    done();
+  });
 });

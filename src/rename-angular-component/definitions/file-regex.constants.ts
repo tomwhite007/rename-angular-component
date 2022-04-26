2c326893bfa36c0f1425b7dde0f334087bca7148
Tom White <tomwhite007@gmail.com>
2022-04-26 16:34:35 +0100

    fix: anyConstructRegexPartial

--- src/rename-angular-component/definitions/file-regex.constants.ts
+++ src/rename-angular-component/definitions/file-regex.constants.ts
@@ -2,7 +2,7 @@ export const componentRegexPartial = `(?=\\.component\\.(spec\\.ts|scss|css|sass
 export const directiveRegexPartial = `(?=\\.directive\\.(spec.ts|ts)$)`;
 export const serviceRegexPartial = `(?=\\.service\\.(spec.ts|ts)$)`;
 export const guardRegexPartial = `(?=\\.guard\\.(spec.ts|ts)$)`;
-export const anyConstructRegexPartial = `(?=.+\\.(spec\.ts|scss|css|sass|less|html|ts)$)`;
+export const anyConstructRegexPartial = `(?=\\.[\\w\\-_]+\\.(spec\\.ts|scss|css|sass|less|html|ts)$)`;
 export const likeFilesRegexPartialLookup: { [key: string]: string } = {
   component: componentRegexPartial,
   directive: directiveRegexPartial,

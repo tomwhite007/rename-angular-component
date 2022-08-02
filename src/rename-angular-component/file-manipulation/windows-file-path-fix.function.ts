fe5b11b4f91f079ef3bb42be38233605a84c595c
Tom White <tomwhite007@gmail.com>
2022-08-02 09:24:11 +0100

    feat: force windows drive letter to lower case

--- src/rename-angular-component/file-manipulation/windows-file-path-fix.function.ts
+++ src/rename-angular-component/file-manipulation/windows-file-path-fix.function.ts
@@ -1,6 +1,10 @@
 export function windowsFilePathFix(path: string, backSlash = false) {
   const windowsDriveMatch = path.match(/^\/[a-z]:/i) || path.match(/^[a-z]:/i);
   if (windowsDriveMatch) {
+    path = path.replace(
+      windowsDriveMatch[0],
+      windowsDriveMatch[0].toLocaleLowerCase()
+    );
     path = path.replace(/^\//, '');
     if (backSlash) {
       path = path.replace(/\//g, '\\');

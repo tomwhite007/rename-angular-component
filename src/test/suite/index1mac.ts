25d384ecaa6ddc05872491d87d8b9b9a7bf869f2
Tom White <tomwhite007@gmail.com>
2024-02-22 09:51:06 +0000

    test: dup scenarios for both oses

---
+++ src/test/suite/index1mac.ts
@@ -0,0 +1,5 @@
+import { runMochaScenarioFile } from './helpers/run-mocha-scenario-file.function';
+
+export function run(): Promise<void> {
+  return runMochaScenarioFile('./suite/scenario1-mac.test.js');
+}

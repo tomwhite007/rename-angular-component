629edb335a6b08eb564e8cbe11e7179090340d29
Tom White <tomwhite007@gmail.com>
2024-02-23 08:32:50 +0000

    feat: windows scenario 2 working

---
+++ src/test/suite/scenario2-windows.test.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+import { genericTestScenario } from './helpers/generic-test-scenario.function';
+
+suite('Suite Scenario 2', () => {
+  vscode.window.showInformationMessage('Start all tests.');
+
+  test('Scenario 2', async () => {
+    /*
+    This test scenario uses a private repo as the seed to base 1 renamer test on.
+    See src/test/suite/scenario1.test.ts and src/test/suite/scenario5.test.ts for publicly available test repos.
+    */
+
+    await genericTestScenario({
+      projectRoot: 'C:\\Development\\my-stuff\\rename-in-shop-test',
+      renames: [
+        {
+          filePath:
+            './apps/Shop/src/app/registration/registration.component.scss',
+          construct: 'component',
+          newStub: 'tom-test',
+        },
+      ],
+      fileDiffPath: './src/test/suite/diffs/shp-wild-paths.txt',
+    });
+  });
+});

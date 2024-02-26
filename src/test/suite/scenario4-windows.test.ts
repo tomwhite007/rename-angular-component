fb08ffb53fcf8a4436793cee7d10c8fd98d36099
Tom White <tomwhite007@gmail.com>
2024-02-26 07:51:32 +0000

    test: scenario 4 windows working

---
+++ src/test/suite/scenario4-windows.test.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+import { genericTestScenario } from './helpers/generic-test-scenario.function';
+
+suite('Suite Scenario 4', () => {
+  vscode.window.showInformationMessage('Start all tests.');
+
+  test('Scenario 4', async () => {
+    /*
+    This test scenario uses a private repo as the seed to base 1 renamer test on.
+    See src/test/suite/scenario1.test.ts and src/test/suite/scenario5.test.ts for publicly available test repos.
+    */
+
+    await genericTestScenario({
+      projectRoot: 'C:\\Development\\my-stuff\\test-paths-app',
+      renames: [
+        {
+          filePath:
+            './projects/my-service-lib/src/lib/my-service-lib.component.ts',
+          construct: 'service',
+          newStub: 'tom-test',
+        },
+      ],
+      fileDiffPath: './src/test/suite/diffs/test-paths-app.txt',
+    });
+  });
+});

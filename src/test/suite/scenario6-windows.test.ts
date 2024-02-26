006d796667d0b69177886f4c6068c0653f384045
Tom White <tomwhite007@gmail.com>
2024-02-26 07:56:56 +0000

    test: scenario 6 windows working

---
+++ src/test/suite/scenario6-windows.test.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+import { genericTestScenario } from './helpers/generic-test-scenario.function';
+
+suite('Suite Scenario 6', () => {
+  vscode.window.showInformationMessage('Start all tests.');
+
+  test('Scenario 6', async () => {
+    /*
+    This test scenario uses publicly available repo: https://github.com/tomwhite007/test-latest-css
+    as the seed to base 1 renamer test on.
+    */
+
+    await genericTestScenario({
+      projectRoot: 'C:\\Development\\my-stuff\\test-ng17-css',
+      renames: [
+        {
+          filePath: './src/app/app.component.css',
+          construct: 'component',
+          newStub: 'tom-test',
+        },
+      ],
+      fileDiffPath:
+        './src/test/suite/diffs/test-styleurl-and-jasminespy-app.txt',
+    });
+  });
+});

bc8749ad2b1b905f3a6a9fd5c61ab678e6381880
Tom White <tomwhite007@gmail.com>
2025-05-02 09:09:33 +0100

    test: custom-edits

---
+++ src/test/unit-tests/custom-edits.test.ts
@@ -0,0 +1,169 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const {
+  getClassNameEdits,
+  getCoreClassEdits,
+  SelectorTransfer,
+} = require('../../rename-angular-component/in-file-edits/custom-edits');
+import { describe, it } from 'mocha';
+
+describe('Custom Edits', () => {
+  describe('getCoreClassEdits', () => {
+    it('should handle class name changes and singular styleUrl', () => {
+      const sourceText = `
+        @Component({
+          selector: 'app-old',
+          templateUrl: './old.component.html',
+          styleUrl: './old.component.scss'
+        })
+        export class OldComponent {
+          @Input() old: string;
+        }
+      `;
+
+      const selectorTransfer = new SelectorTransfer();
+      const edits = getCoreClassEdits(
+        'OldComponent',
+        'NewComponent',
+        'old',
+        'new',
+        'component',
+        selectorTransfer
+      )('test.ts', sourceText);
+
+      expect(edits).to.have.length(4);
+      expect(edits[0].replacement).to.equal("'app-new'");
+      expect(edits[1].replacement).to.equal("'./new.component.html'");
+      expect(edits[2].replacement).to.equal("'./new.component.scss'");
+      expect(edits[3].replacement).to.equal('NewComponent');
+    });
+
+    it('should handle attribute selectors', () => {
+      const sourceText = `
+        @Component({
+          selector: '[app-old]',
+          templateUrl: './old.component.html'
+        })
+        export class OldComponent {
+          @Input() old: string;
+        }
+      `;
+
+      const selectorTransfer = new SelectorTransfer();
+      const edits = getCoreClassEdits(
+        'OldComponent',
+        'NewComponent',
+        'old',
+        'new',
+        'component',
+        selectorTransfer
+      )('test.ts', sourceText);
+
+      expect(edits).to.have.length(3);
+      expect(edits[0].replacement).to.equal("'[appNew]'");
+      expect(edits[1].replacement).to.equal("'./new.component.html'");
+      expect(edits[2].replacement).to.equal('NewComponent');
+    });
+
+    it('should handle multiple styleUrls', () => {
+      const sourceText = `
+        @Component({
+          selector: 'app-old',
+          styleUrls: ['./old.component.scss', './old.component.css']
+        })
+        export class OldComponent {}
+      `;
+
+      const selectorTransfer = new SelectorTransfer();
+      const edits = getCoreClassEdits(
+        'OldComponent',
+        'NewComponent',
+        'old',
+        'new',
+        'component',
+        selectorTransfer
+      )('test.ts', sourceText);
+
+      expect(edits).to.have.length(4);
+      expect(edits[0].replacement).to.equal("'app-new'");
+      expect(edits[1].replacement).to.equal("'./new.component.scss'");
+      expect(edits[2].replacement).to.equal("'./new.component.css'");
+      expect(edits[3].replacement).to.equal('NewComponent');
+    });
+  });
+
+  describe('getClassNameEdits', () => {
+    it('should replace class name in imports', () => {
+      const sourceText = `
+        import { OldComponent } from './old.component';
+        import { OldComponent as Alias } from './old.component';
+      `;
+
+      const edits = getClassNameEdits('OldComponent', 'NewComponent')(
+        'test.ts',
+        sourceText
+      );
+
+      expect(edits).to.have.length(2);
+      expect(edits[0].replacement).to.equal('NewComponent');
+      expect(edits[1].replacement).to.equal('NewComponent');
+    });
+
+    it('should replace class name in type references and instances', () => {
+      const sourceText = `
+        const component: OldComponent = new OldComponent();
+        function test(param: OldComponent): OldComponent {
+          return param;
+        }
+      `;
+
+      const edits = getClassNameEdits('OldComponent', 'NewComponent')(
+        'test.ts',
+        sourceText
+      );
+
+      expect(edits).to.have.length(4);
+      edits.forEach((edit: { replacement: string }) => {
+        expect(edit.replacement).to.equal('NewComponent');
+      });
+    });
+
+    it('should not replace partial matches', () => {
+      const sourceText = `
+        class NotOldComponent {}
+        const oldComponent = new NotOldComponent();
+      `;
+
+      const edits = getClassNameEdits('OldComponent', 'NewComponent')(
+        'test.ts',
+        sourceText
+      );
+
+      expect(edits).to.have.length(0);
+    });
+  });
+
+  describe('SelectorTransfer', () => {
+    it('should store old and new selectors', () => {
+      const sourceText = `
+        @Component({
+          selector: 'app-old'
+        })
+        export class OldComponent {}
+      `;
+
+      const selectorTransfer = new SelectorTransfer();
+      getCoreClassEdits(
+        'OldComponent',
+        'NewComponent',
+        'old',
+        'new',
+        'component',
+        selectorTransfer
+      )('test.ts', sourceText);
+
+      expect(selectorTransfer.oldSelector).to.equal('app-old');
+      expect(selectorTransfer.newSelector).to.equal('app-new');
+    });
+  });
+});

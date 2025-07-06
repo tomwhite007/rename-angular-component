be436c40cb8b88befb3a1e0c7f671205b0802c9e
Tom White <tomwhite007@gmail.com>
2025-07-06 21:44:10 +0100

    test: both ng20 and not scenario 1

--- src/test/suite/scenario1-mac.test.ts
+++ src/test/suite/scenario1-mac.test.ts
@@ -4,7 +4,7 @@ import { genericTestScenario } from './helpers/generic-test-scenario.function';
 suite('Suite Scenario 1', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Scenario 1', async () => {
+  test('Scenario 1 ng20', async () => {
     /*
     This test scenario uses publicly available repo: https://github.com/tomwhite007/simple-reactive-viewmodel-example
     as the seed to base 1 renamer test on.
@@ -24,6 +24,29 @@ suite('Suite Scenario 1', () => {
       fileDiffPath:
         './src/test/suite/diffs/simple-reactive-viewmodel-example.txt',
       useNg20Convention: true,
+    });
+  });
+
+  test('Scenario 1 non-ng20', async () => {
+    /*
+    This test scenario uses publicly available repo: https://github.com/tomwhite007/simple-reactive-viewmodel-example
+    as the seed to base 1 renamer test on.
+    */
+
+    await genericTestScenario({
+      projectRoot:
+        '/Users/tom/Development/vscode-ext/_rename-test-spas/simple-reactive-viewmodel-example',
+      renames: [
+        {
+          filePath:
+            './src/app/shared/book-ui/book-list/book-list.component.html',
+          construct: 'file',
+          newFilenameInput: 'tom-test',
+        },
+      ],
+      fileDiffPath:
+        './src/test/suite/diffs/simple-reactive-viewmodel-example.txt',
+      useNg20Convention: false,
     });
   });
 });

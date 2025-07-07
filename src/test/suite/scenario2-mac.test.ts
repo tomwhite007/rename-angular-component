26ea0f52903ce8b9f91bc08028265a0f5b17bbd1
Tom White <tomwhite007@gmail.com>
2025-07-07 20:25:41 +0100

    test: scenario 2 both

--- src/test/suite/scenario2-mac.test.ts
+++ src/test/suite/scenario2-mac.test.ts
@@ -4,7 +4,7 @@ import { genericTestScenario } from './helpers/generic-test-scenario.function';
 suite('Suite Scenario 2', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Scenario 2', async () => {
+  test('Scenario 2 ng20', async () => {
     /*
     This test scenario uses a private repo as the seed to base 1 renamer test on.
     See src/test/suite/scenario1.test.ts and src/test/suite/scenario5.test.ts for publicly available test repos.
@@ -23,6 +23,28 @@ suite('Suite Scenario 2', () => {
       ],
       fileDiffPath: './src/test/suite/diffs/shp-wild-paths.txt',
       useNg20Convention: true,
+    });
+  });
+
+  test.only('Scenario 2 non-ng20', async () => {
+    /*
+    This test scenario uses a private repo as the seed to base 1 renamer test on.
+    See src/test/suite/scenario1.test.ts and src/test/suite/scenario5.test.ts for publicly available test repos.
+    */
+
+    await genericTestScenario({
+      projectRoot:
+        '/Users/tom/Development/vscode-ext/_rename-test-spas/shp-wild-paths',
+      renames: [
+        {
+          filePath:
+            './apps/Shop/src/app/registration/registration.component.scss',
+          construct: 'component',
+          newFilenameInput: 'tom-test.component',
+        },
+      ],
+      fileDiffPath: './src/test/suite/diffs/shp-wild-paths.txt',
+      useNg20Convention: false,
     });
   });
 });

9eb64467aa04056f92c097993799975698fe6a1e
Tom White <tomwhite007@gmail.com>
2025-07-07 21:08:46 +0100

    test: update scenario2

--- src/test/suite/scenario2-mac.test.ts
+++ src/test/suite/scenario2-mac.test.ts
@@ -25,26 +25,4 @@ suite('Suite Scenario 2', () => {
       useNg20Convention: true,
     });
   });
-
-  test.only('Scenario 2 non-ng20', async () => {
-    /*
-    This test scenario uses a private repo as the seed to base 1 renamer test on.
-    See src/test/suite/scenario1.test.ts and src/test/suite/scenario5.test.ts for publicly available test repos.
-    */
-
-    await genericTestScenario({
-      projectRoot:
-        '/Users/tom/Development/vscode-ext/_rename-test-spas/shp-wild-paths',
-      renames: [
-        {
-          filePath:
-            './apps/Shop/src/app/registration/registration.component.scss',
-          construct: 'component',
-          newFilenameInput: 'tom-test.component',
-        },
-      ],
-      fileDiffPath: './src/test/suite/diffs/shp-wild-paths.txt',
-      useNg20Convention: false,
-    });
-  });
 });

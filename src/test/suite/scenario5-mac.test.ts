53e4b79294145021fc01cd0f31316a40bf025f32
Tom White <tomwhite007@gmail.com>
2025-07-10 20:54:45 +0100

    test: create scenario 5 for non ng20 as 2nd run

--- src/test/suite/scenario5-mac.test.ts
+++ src/test/suite/scenario5-mac.test.ts
@@ -4,7 +4,7 @@ import { genericTestScenario } from './helpers/generic-test-scenario.function';
 suite('Suite Scenario 5', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Scenario 5', async () => {
+  test('Scenario 5 ng20', async () => {
     /*
     This test scenario uses publicly available repo: https://github.com/tomwhite007/test-rename-spa
     as the seed to base 3 renamer tests on.
@@ -42,8 +42,51 @@ suite('Suite Scenario 5', () => {
           newFilenameInput: 'formatter',
         },
       ],
-      fileDiffPath: './src/test/suite/diffs/test-rename-spa.txt',
+      fileDiffPath: './src/test/suite/diffs/test-rename-spa-ng20.txt',
       useNg20Convention: true,
+    });
+  });
+
+  test('Scenario 5 not ng20', async () => {
+    /*
+    This test scenario uses publicly available repo: https://github.com/tomwhite007/test-rename-spa
+    as the seed to base 3 renamer tests on.
+    */
+
+    await genericTestScenario({
+      projectRoot:
+        '/Users/tom/Development/vscode-ext/_rename-test-spas/test-rename-spa',
+      renames: [
+        {
+          filePath: './src/app/products/products.component.spec.ts',
+          construct: 'component',
+          newFilenameInput: 'tom-test.component',
+        },
+        {
+          filePath:
+            './projects/shared/src/lib/multiply-by-ten/multiply-by-ten.directive.ts',
+          construct: 'directive',
+          newFilenameInput: 'dir-prop.directive',
+        },
+        {
+          filePath:
+            './projects/shared/src/lib/multiply-by-twenty/multiply-by-twenty.directive.spec.ts',
+          construct: 'directive',
+          newFilenameInput: 'dir-identifier.directive',
+        },
+        {
+          filePath: './src/app/tom-test/products.module.ts',
+          construct: 'module',
+          newFilenameInput: 'tom-test.module',
+        },
+        {
+          filePath: './src/app/tom-test/product.pipe.ts',
+          construct: 'file',
+          newFilenameInput: 'formatter',
+        },
+      ],
+      fileDiffPath: './src/test/suite/diffs/test-rename-spa-non-ng20.txt',
+      useNg20Convention: false,
     });
   });
 });

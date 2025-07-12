8f8e4a828c05f92052a2d58d521ff0b3b79f1405
Tom White <tomwhite007@gmail.com>
2025-07-12 21:41:58 +0100

    refactor: getCoreFileDefinitionDetails

--- src/rename-angular-component/in-file-edits/get-core-file-definition-details.function.ts
+++ src/rename-angular-component/in-file-edits/get-core-file-definition-details.function.ts
@@ -6,120 +6,164 @@ import {
   DefinitionType,
 } from '../definitions/file.interfaces';
 
+// Type guards for different TypeScript node types
+const typeGuards = {
+  isClassDeclaration: (node: ts.Statement): node is ts.ClassDeclaration =>
+    ts.isClassDeclaration(node),
+
+  isFunctionDeclaration: (node: ts.Statement): node is ts.FunctionDeclaration =>
+    ts.isFunctionDeclaration(node),
+
+  isVariableStatement: (node: ts.Statement): node is ts.VariableStatement =>
+    ts.isVariableStatement(node),
+
+  isInterfaceDeclaration: (
+    node: ts.Statement
+  ): node is ts.InterfaceDeclaration => ts.isInterfaceDeclaration(node),
+
+  isEnumDeclaration: (node: ts.Statement): node is ts.EnumDeclaration =>
+    ts.isEnumDeclaration(node),
+};
+
+// Helper functions for extracting names from nodes
+const nameExtractors = {
+  getClassName: (node: ts.ClassDeclaration): string =>
+    node.name?.escapedText?.toString() ?? '',
+
+  getFunctionName: (node: ts.FunctionDeclaration): string =>
+    node.name?.escapedText?.toString() ?? '',
+
+  getVariableName: (node: ts.VariableStatement): string => {
+    if (node.declarationList.declarations.length > 0) {
+      const variable = node.declarationList.declarations[0];
+      if (ts.isIdentifier(variable.name)) {
+        return variable.name.escapedText.toString();
+      }
+    }
+    return '';
+  },
+
+  getInterfaceName: (node: ts.InterfaceDeclaration): string =>
+    node.name?.escapedText?.toString() ?? '',
+
+  getEnumName: (node: ts.EnumDeclaration): string =>
+    node.name?.escapedText?.toString() ?? '',
+};
+
+// Definition processor interface
+interface DefinitionProcessor {
+  type: DefinitionType;
+  predicate: (node: ts.Statement, stub: string) => boolean;
+  extractName: (node: ts.Statement) => string;
+  extractDecorator?: (node: ts.Statement) => string;
+}
+
+// Definition processors map
+const definitionProcessors: DefinitionProcessor[] = [
+  {
+    type: 'class',
+    predicate: (node: ts.Statement, stub: string) =>
+      typeGuards.isClassDeclaration(node) &&
+      hasDecorator(node) &&
+      definitionStartsWithStub(nameExtractors.getClassName(node), stub),
+    extractName: (node: ts.Statement) =>
+      nameExtractors.getClassName(node as ts.ClassDeclaration),
+    extractDecorator: (node: ts.Statement) =>
+      getDecoratorName(node as ts.ClassDeclaration),
+  },
+  {
+    type: 'class',
+    predicate: (node: ts.Statement, stub: string) =>
+      typeGuards.isClassDeclaration(node) &&
+      !hasDecorator(node) &&
+      definitionStartsWithStub(nameExtractors.getClassName(node), stub),
+    extractName: (node: ts.Statement) =>
+      nameExtractors.getClassName(node as ts.ClassDeclaration),
+  },
+  {
+    type: 'function',
+    predicate: (node: ts.Statement, stub: string) =>
+      typeGuards.isFunctionDeclaration(node) &&
+      definitionStartsWithStub(nameExtractors.getFunctionName(node), stub),
+    extractName: (node: ts.Statement) =>
+      nameExtractors.getFunctionName(node as ts.FunctionDeclaration),
+  },
+  {
+    type: 'variable',
+    predicate: (node: ts.Statement, stub: string) =>
+      typeGuards.isVariableStatement(node) &&
+      definitionStartsWithStub(nameExtractors.getVariableName(node), stub),
+    extractName: (node: ts.Statement) =>
+      nameExtractors.getVariableName(node as ts.VariableStatement),
+  },
+  {
+    type: 'interface',
+    predicate: (node: ts.Statement, stub: string) =>
+      typeGuards.isInterfaceDeclaration(node) &&
+      definitionStartsWithStub(nameExtractors.getInterfaceName(node), stub),
+    extractName: (node: ts.Statement) =>
+      nameExtractors.getInterfaceName(node as ts.InterfaceDeclaration),
+  },
+  {
+    type: 'enum',
+    predicate: (node: ts.Statement, stub: string) =>
+      typeGuards.isEnumDeclaration(node) &&
+      definitionStartsWithStub(nameExtractors.getEnumName(node), stub),
+    extractName: (node: ts.Statement) =>
+      nameExtractors.getEnumName(node as ts.EnumDeclaration),
+  },
+];
+
 export async function getCoreFileDefinitionDetails(
   filepath: string,
   stub: string
 ): Promise<CoreFileDefinitionDetails | null> {
-  const sourceText = await fs.readFileAsync(filepath, 'utf-8');
+  try {
+    const sourceText = await fs.readFileAsync(filepath, 'utf-8');
+    const file = ts.createSourceFile(
+      filepath,
+      sourceText,
+      ts.ScriptTarget.Latest
+    );
 
-  const file = ts.createSourceFile(
-    filepath,
-    sourceText,
-    ts.ScriptTarget.Latest
-  );
-
-  let definitionName = '';
-  let definitionType: DefinitionType = null;
-  let decoratorName = '';
-
-  const classAndDecoratorDeclaration = file.statements.find((node) =>
-    isClassAndStartsWithStubWithDecorator(node, stub)
-  );
-  if (classAndDecoratorDeclaration) {
-    const classDeclaration =
-      classAndDecoratorDeclaration as ts.ClassDeclaration;
-    definitionType = 'class';
-    definitionName = classDeclaration.name?.escapedText ?? '';
-    decoratorName = getDecoratorName(classDeclaration);
-    return {
-      definitionName,
-      definitionType,
-      decoratorName,
-    };
+    return findDefinitionInStatements(file.statements, stub);
+  } catch (error) {
+    console.error(`Error reading file ${filepath}:`, error);
+    return null;
   }
+}
 
-  const classDeclaration = file.statements.find((node) =>
-    isClassAndStartsWithStub(node, stub)
-  );
-  if (classDeclaration) {
-    const classNode = classDeclaration as ts.ClassDeclaration;
-    definitionType = 'class';
-    definitionName = classNode.name?.escapedText ?? '';
-    return {
-      definitionName,
-      definitionType,
-      decoratorName: '',
-    };
-  }
+function findDefinitionInStatements(
+  statements: ts.NodeArray<ts.Statement>,
+  stub: string
+): CoreFileDefinitionDetails | null {
+  for (const processor of definitionProcessors) {
+    const matchingNode = statements.find((node) =>
+      processor.predicate(node, stub)
+    );
 
-  const functionDeclaration = file.statements.find((node) =>
-    isFunctionAndStartsWithStub(node, stub)
-  );
-  if (functionDeclaration) {
-    const functionNode = functionDeclaration as ts.FunctionDeclaration;
-    definitionType = 'function';
-    definitionName = functionNode.name?.escapedText ?? '';
-    return {
-      definitionName,
-      definitionType,
-      decoratorName: '',
-    };
-  }
-
-  const variableDeclaration = file.statements.find((node) =>
-    isVariableAndStartsWithStub(node, stub)
-  );
-  if (variableDeclaration) {
-    const variableNode = variableDeclaration as ts.VariableStatement;
-    definitionType = 'variable';
-    definitionName = getVariableName(variableNode);
-    return {
-      definitionName,
-      definitionType,
-      decoratorName: '',
-    };
-  }
-
-  const interfaceDeclaration = file.statements.find((node) =>
-    isInterfaceAndStartsWithStub(node, stub)
-  );
-  if (interfaceDeclaration) {
-    const interfaceNode = interfaceDeclaration as ts.InterfaceDeclaration;
-    definitionType = 'interface';
-    definitionName = interfaceNode.name?.escapedText ?? '';
-    return {
-      definitionName,
-      definitionType,
-      decoratorName: '',
-    };
-  }
-
-  const enumDeclaration = file.statements.find((node) =>
-    isEnumAndStartsWithStub(node, stub)
-  );
-  if (enumDeclaration) {
-    const enumNode = enumDeclaration as ts.EnumDeclaration;
-    definitionType = 'enum';
-    definitionName = enumNode.name?.escapedText ?? '';
-    return {
-      definitionName,
-      definitionType,
-      decoratorName: '',
-    };
+    if (matchingNode) {
+      return {
+        definitionName: processor.extractName(matchingNode),
+        definitionType: processor.type,
+        decoratorName: processor.extractDecorator?.(matchingNode) ?? '',
+      };
+    }
   }
 
   console.log('No definition found to rename in file');
-
   return null;
 }
 
+function hasDecorator(node: ts.ClassDeclaration): boolean {
+  return getDecoratorName(node).length > 0;
+}
+
 function getDecoratorName(node: ts.ClassDeclaration): string {
-  const decorator: any = node.modifiers?.find((decorator) => {
-    if (ts.isDecorator(decorator)) {
-      return true;
-    }
-    return false;
-  });
+  const decorator = node.modifiers?.find((modifier) =>
+    ts.isDecorator(modifier)
+  );
+
   if (
     decorator &&
     ts.isDecorator(decorator) &&
@@ -128,100 +172,22 @@ function getDecoratorName(node: ts.ClassDeclaration): string {
   ) {
     return decorator.expression.expression.text;
   }
-  return '';
-}
 
-function isClassAndStartsWithStub(node: ts.Statement, stub: string): boolean {
-  const isClass = ts.isClassDeclaration(node);
-  if (!isClass) {
-    return false;
-  }
-  const className = isClass ? node.name?.escapedText.toString() ?? '' : '';
-  return definitionStartsWithStub(className, stub);
-}
-
-function isClassAndStartsWithStubWithDecorator(
-  node: ts.Statement,
-  stub: string
-): boolean {
-  if (!isClassAndStartsWithStub(node, stub)) {
-    return false;
-  }
-  const classDeclaration = node as ts.ClassDeclaration;
-  const decoratorName = getDecoratorName(classDeclaration);
-  return decoratorName.length > 0;
-}
-
-function isFunctionAndStartsWithStub(
-  node: ts.Statement,
-  stub: string
-): boolean {
-  const isFunction = ts.isFunctionDeclaration(node);
-  if (!isFunction) {
-    return false;
-  }
-  const functionName = isFunction
-    ? node.name?.escapedText.toString() ?? ''
-    : '';
-  return definitionStartsWithStub(functionName, stub);
-}
-
-function isVariableAndStartsWithStub(
-  node: ts.Statement,
-  stub: string
-): boolean {
-  if (ts.isVariableStatement(node)) {
-    const variableName = getVariableName(node);
-    if (definitionStartsWithStub(variableName, stub)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function isInterfaceAndStartsWithStub(
-  node: ts.Statement,
-  stub: string
-): boolean {
-  const isInterface = ts.isInterfaceDeclaration(node);
-  if (!isInterface) {
-    return false;
-  }
-  const interfaceName = isInterface
-    ? node.name?.escapedText.toString() ?? ''
-    : '';
-  return definitionStartsWithStub(interfaceName, stub);
-}
-
-function isEnumAndStartsWithStub(node: ts.Statement, stub: string): boolean {
-  const isEnum = ts.isEnumDeclaration(node);
-  if (!isEnum) {
-    return false;
-  }
-  const enumName = isEnum ? node.name?.escapedText.toString() ?? '' : '';
-  return definitionStartsWithStub(enumName, stub);
-}
-
-function getVariableName(node: ts.VariableStatement): string {
-  if (node.declarationList.declarations.length > 0) {
-    const variable = node.declarationList.declarations[0];
-    if (ts.isIdentifier(variable.name)) {
-      return variable.name.escapedText.toString();
-    }
-  }
   return '';
 }
 
 function definitionStartsWithStub(
-  coreClassOrFunctionName: string,
+  definitionName: string,
   stub: string
 ): boolean {
-  if (stub.length === 0) {
+  if (stub.length === 0 || definitionName.length === 0) {
     return false;
   }
-  const dasherizedName = dasherize(coreClassOrFunctionName);
+
+  const dasherizedName = dasherize(definitionName);
   const stubDashCount = stub.split('-').length;
   const nameDashCount = dasherizedName.split('-').length;
+
   return (
     dasherizedName.startsWith(stub) &&
     // allow one extra word for construct

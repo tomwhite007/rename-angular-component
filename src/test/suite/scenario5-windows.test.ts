680cdb232af18d32fb19a22e79770ae8715ca46e
Tom White <tomwhite007@gmail.com>
2025-08-17 17:22:11 +0100

    test: win scenario 5 mirrored

--- src/test/suite/scenario5-windows.test.ts
+++ src/test/suite/scenario5-windows.test.ts
@@ -4,39 +4,86 @@ import { genericTestScenario } from './helpers/generic-test-scenario.function';
 suite('Suite Scenario 5', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Scenario 5', async () => {
+  test('Scenario 5 ng20', async () => {
     /*
     This test scenario uses publicly available repo: https://github.com/tomwhite007/test-rename-spa
     as the seed to base 3 renamer tests on.
     */
 
     await genericTestScenario({
-      projectRoot: 'C:/Development/my-stuff/test-rename-spa',
+      projectRoot: 'C:\\Development\\my-stuff\\test-rename-spa',
       renames: [
         {
           filePath: './src/app/products/products.component.spec.ts',
           construct: 'component',
-          newFilenameInput: 'tom-test',
+          newFilenameInput: 'tom-test.component',
         },
         {
           filePath:
             './projects/shared/src/lib/multiply-by-ten/multiply-by-ten.directive.ts',
           construct: 'directive',
-          newFilenameInput: 'dir-prop',
+          newFilenameInput: 'dir-prop.directive',
         },
         {
           filePath:
             './projects/shared/src/lib/multiply-by-twenty/multiply-by-twenty.directive.spec.ts',
           construct: 'directive',
-          newFilenameInput: 'dir-identifier',
+          newFilenameInput: 'dir-identifier.directive',
+        },
+        {
+          filePath: './src/app/tom-test.component/products.module.ts',
+          construct: 'module',
+          newFilenameInput: 'tom-test.module',
+        },
+        {
+          filePath: './src/app/tom-test.component/product.pipe.ts',
+          construct: 'file',
+          newFilenameInput: 'formatter',
+        },
+      ],
+      fileDiffPath: './src/test/suite/diffs/test-rename-spa-ng20.txt',
+      useNg20Convention: true,
+    });
+  });
+
+  test('Scenario 5 non ng20', async () => {
+    /*
+    This test scenario uses publicly available repo: https://github.com/tomwhite007/test-rename-spa
+    as the seed to base 3 renamer tests on.
+    */
+
+    await genericTestScenario({
+      projectRoot: 'C:\\Development\\my-stuff\\test-rename-spa',
+      renames: [
+        {
+          filePath: './src/app/products/products.component.spec.ts',
+          construct: 'component',
+          newFilenameInput: 'tom-test.component',
+        },
+        {
+          filePath:
+            './projects/shared/src/lib/multiply-by-ten/multiply-by-ten.directive.ts',
+          construct: 'directive',
+          newFilenameInput: 'dir-prop.directive',
+        },
+        {
+          filePath:
+            './projects/shared/src/lib/multiply-by-twenty/multiply-by-twenty.directive.spec.ts',
+          construct: 'directive',
+          newFilenameInput: 'dir-identifier.directive',
         },
         {
           filePath: './src/app/tom-test/products.module.ts',
           construct: 'module',
-          newFilenameInput: 'tom-test',
+          newFilenameInput: 'tom-test.module',
+        },
+        {
+          filePath: './src/app/tom-test/product.pipe.ts',
+          construct: 'file',
+          newFilenameInput: 'formatter',
         },
       ],
-      fileDiffPath: './src/test/suite/diffs/test-rename-spa.txt',
+      fileDiffPath: './src/test/suite/diffs/test-rename-spa-non-ng20.txt',
       useNg20Convention: false,
     });
   });

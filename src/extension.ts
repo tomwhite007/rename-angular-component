b8ee45e323229434a1990581ff40cc2641f4336c
Tom White <tomwhite007@gmail.com>
2025-08-20 07:52:59 +0100

    feat: check isAngular project before enabling context menus

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
+import fs from 'fs-extra-promise';
 import vscode from 'vscode';
 
+import path from 'path';
 import { ReferenceIndexBuilder } from './move-ts-indexer/reference-index-builder';
 import { conf } from './move-ts-indexer/util/helper-functions';
 import { EXTENSION_NAME } from './rename-angular-component/definitions/extension-name';
@@ -9,81 +11,127 @@ import { UserMessage } from './rename-angular-component/logging/user-message.cla
 import { Renamer } from './rename-angular-component/renamer.class';
 
 export function activate(context: vscode.ExtensionContext) {
-  const debugLogger = new DebugLogger(conf('debugLog', false));
-  const indexStart = Date.now();
-  const userMessage = new UserMessage(EXTENSION_NAME);
-  const indexer: ReferenceIndexBuilder = new ReferenceIndexBuilder(debugLogger);
-  const fileMoveHandler = new FileMoveHandler(indexer, userMessage);
-
-  const initWithProgress = () => {
-    return vscode.window.withProgress(
-      {
-        location: vscode.ProgressLocation.Notification,
-        title: `${EXTENSION_NAME} is indexing`,
-      },
-      async (progress) => {
-        return indexer.init(progress);
-      }
+  isAngularProject().then((isAngular) => {
+    const debugLogger = new DebugLogger(conf('debugLog', false));
+    debugLogger.logToConsole(
+      `Rename Angular Component check: is Angular project? ${isAngular}`
     );
-  };
 
-  const initialise = async () => {
-    if (indexer.isinitialised) {
-      return Promise.resolve();
+    // Set context key to enable / disable commands listed in package.json
+    vscode.commands.executeCommand(
+      'setContext',
+      'renameAngularComponent.isAngularProject',
+      isAngular
+    );
+
+    if (!isAngular) {
+      return;
     }
-    await initWithProgress();
-    return (Date.now() - indexStart) / 1000;
-  };
 
-  const initialisePromise = initialise();
-  const renamer = new Renamer(
-    initialisePromise,
-    userMessage,
-    debugLogger,
-    fileMoveHandler
-  );
+    const indexStart = Date.now();
+    const userMessage = new UserMessage(EXTENSION_NAME);
+    const indexer: ReferenceIndexBuilder = new ReferenceIndexBuilder(
+      debugLogger
+    );
+    const fileMoveHandler = new FileMoveHandler(indexer, userMessage);
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'rename-angular-component.renameComponent',
-      async (uri: vscode.Uri) => renamer.rename('component', uri)
-    )
-  );
+    const initWithProgress = () => {
+      return vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: `${EXTENSION_NAME} is indexing`,
+        },
+        async (progress) => {
+          return indexer.init(progress);
+        }
+      );
+    };
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'rename-angular-component.renameDirective',
-      (uri: vscode.Uri) => renamer.rename('directive', uri)
-    )
-  );
+    const initialise = async () => {
+      if (indexer.isinitialised) {
+        return Promise.resolve();
+      }
+      await initWithProgress();
+      return (Date.now() - indexStart) / 1000;
+    };
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'rename-angular-component.renameService',
-      (uri: vscode.Uri) => renamer.rename('service', uri)
-    )
-  );
+    const initialisePromise = initialise();
+    const renamer = new Renamer(
+      initialisePromise,
+      userMessage,
+      debugLogger,
+      fileMoveHandler
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'rename-angular-component.renameGuard',
-      (uri: vscode.Uri) => renamer.rename('guard', uri)
-    )
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'rename-angular-component.renameComponent',
+        async (uri: vscode.Uri) => renamer.rename('component', uri)
+      )
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'rename-angular-component.renameModule',
-      (uri: vscode.Uri) => renamer.rename('module', uri)
-    )
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'rename-angular-component.renameDirective',
+        (uri: vscode.Uri) => renamer.rename('directive', uri)
+      )
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'rename-angular-component.renameFile',
-      (uri: vscode.Uri) => renamer.rename('file', uri)
-    )
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'rename-angular-component.renameService',
+        (uri: vscode.Uri) => renamer.rename('service', uri)
+      )
+    );
+
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'rename-angular-component.renameGuard',
+        (uri: vscode.Uri) => renamer.rename('guard', uri)
+      )
+    );
+
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'rename-angular-component.renameModule',
+        (uri: vscode.Uri) => renamer.rename('module', uri)
+      )
+    );
+
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'rename-angular-component.renameFile',
+        (uri: vscode.Uri) => renamer.rename('file', uri)
+      )
+    );
+  });
 }
 
 export function deactivate() {}
+
+// Angular project detection
+async function isAngularProject(): Promise<boolean> {
+  const folders = vscode.workspace.workspaceFolders;
+  if (!folders) {
+    return false;
+  }
+  for (const folder of folders) {
+    const angularJson = path.join(folder.uri.fsPath, 'angular.json');
+    const packageJson = path.join(folder.uri.fsPath, 'package.json');
+    if (fs.existsSync(angularJson)) {
+      return true;
+    }
+    if (fs.existsSync(packageJson)) {
+      try {
+        const pkg = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
+        if (
+          (pkg.dependencies && pkg.dependencies['@angular/core']) ||
+          (pkg.devDependencies && pkg.devDependencies['@angular/core'])
+        ) {
+          return true;
+        }
+      } catch {}
+    }
+  }
+  return false;
+}

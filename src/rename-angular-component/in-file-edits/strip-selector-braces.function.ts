fb850ef1cd1a8257655d441076fb1e06b6220cbc
Tom White <tomwhite007@gmail.com>
2026-01-08 21:06:26 +0000

    feat: find and replace attribute and css class selectors

--- src/rename-angular-component/in-file-edits/strip-selector-braces.function.ts
+++ src/rename-angular-component/in-file-edits/strip-selector-braces.function.ts
@@ -1,6 +1,23 @@
 export function stripSelectorBraces(selector: string, andQuotes = false) {
+  let result = selector;
+  
+  // Remove selector type markers:
+  // - Leading [ and trailing ] for attribute selectors (even if inside quotes)
+  // - Leading . for class selectors (even if inside quotes)
+  // But preserve dots/brackets that are part of the selector name itself
+  
+  // Find and remove attribute brackets: [selector] -> selector
+  // Use regex to find [ at start (possibly after quotes/spaces) and ] at end
+  result = result.replace(/^([\s']*)\[/, '$1').replace(/\]([\s']*)$/, '$1');
+  
+  // Find and remove class dot prefix: .selector -> selector
+  // Use regex to find . at start (possibly after quotes/spaces)
+  result = result.replace(/^([\s']*)\./, '$1');
+  
+  // Remove quotes if requested (do this last so we can find brackets/dots inside quotes)
   if (andQuotes) {
-    selector = selector.replace(/'/g, '');
+    result = result.replace(/'/g, '');
   }
-  return selector.replace(/(\[|\]|\.)/g, '');
+  
+  return result;
 }
